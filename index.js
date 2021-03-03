5ae97109a7509fee09c51bf25dd45d1714eef8ad
Ken Barlow <kenbrlow@gmail.com>
2021-03-03 12:06:42 -0700

    commit tree a12355bd72052516e77da31a055d466d5b366f13

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(userName, userEmail) {
+
+    let formData = {
+        name: userName,
+        email: userEmail
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    let fetchItem = fetch("http://localhost:3000/users", configObj) 
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(object) {
+                let responseID = object["id"];
+                appendToDom(responseID);
+            })
+            .catch(function(error) {
+                appendToDom(error.message);
+            });
+
+    return fetchItem;
+}
+
+function appendToDom(itemToAppend) {
+    document.body.innerHTML = itemToAppend;
+}
